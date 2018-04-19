54ac8f19add24ef58e5a14073bda163cd749d4b0
Torsten Mandry <torsten.mandry@innoq.com>
2018-04-19 14:28:42 +0200

    define NodeInfo as spring bean with default id value

--- src/main/kotlin/com/innoq/event/blockchainktwebflux/BlockchainKtWebfluxApplication.kt
+++ src/main/kotlin/com/innoq/event/blockchainktwebflux/BlockchainKtWebfluxApplication.kt
@@ -2,35 +2,36 @@ package com.innoq.event.blockchainktwebflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.server.*
-import org.springframework.web.reactive.function.server.RequestPredicates.GET
-import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
-import reactor.core.publisher.Flux
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
+import java.util.*
 
-data class NodeInfo(val id: String)
+data class NodeInfo(val id: String = UUID.randomUUID().toString())
 
 @RestController
-class StandardRestController {
+class StandardRestController(val nodeInfo: NodeInfo) {
     @GetMapping("/standardRestIndex")
-    fun index(): NodeInfo = NodeInfo("4711")
+    fun index(): NodeInfo = nodeInfo
 }
 
 @RestController
-class WebfluxRestController {
+class WebfluxRestController(val nodeInfo: NodeInfo) {
     @GetMapping(value = "/webfluxRestIndex", produces = arrayOf(MediaType.APPLICATION_STREAM_JSON_VALUE))
-    fun index(): Mono<NodeInfo> = Mono.just(NodeInfo("4711"))
+    fun index(): Mono<NodeInfo> = Mono.just(nodeInfo)
 }
 
-class FunctionalHandler {
+class FunctionalHandler(val nodeInfo: NodeInfo) {
     fun index(request: ServerRequest): Mono<ServerResponse>
-            = ok().body(Mono.just(NodeInfo("4711")))
+            = ok().body(Mono.just(nodeInfo))
 }
 
 class Router(val functionalHandler: FunctionalHandler) {
@@ -51,6 +52,8 @@ fun beans() = beans {
     bean {
         Router(ref()).router()
     }
+
+    bean<NodeInfo>()
 }
 
 fun main(args: Array<String>) {
