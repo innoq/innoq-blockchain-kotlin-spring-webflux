c1f688a6d6e4317cb22c770e9d174d49d2326767
rreimann <robert.reimann@googlemail.com>
2018-04-19 14:37:34 +0200

    remove legacy routes, rename handler

--- src/main/kotlin/com/innoq/event/blockchainktwebflux/BlockchainKtWebfluxApplication.kt
+++ src/main/kotlin/com/innoq/event/blockchainktwebflux/BlockchainKtWebfluxApplication.kt
@@ -4,9 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.support.beans
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
@@ -17,27 +14,15 @@ import java.util.*
 
 data class NodeInfo(val id: String = UUID.randomUUID().toString())
 
-@RestController
-class StandardRestController(val nodeInfo: NodeInfo) {
-    @GetMapping("/standardRestIndex")
-    fun index(): NodeInfo = nodeInfo
-}
-
-@RestController
-class WebfluxRestController(val nodeInfo: NodeInfo) {
-    @GetMapping(value = "/webfluxRestIndex", produces = arrayOf(MediaType.APPLICATION_STREAM_JSON_VALUE))
-    fun index(): Mono<NodeInfo> = Mono.just(nodeInfo)
-}
-
-class FunctionalHandler(val nodeInfo: NodeInfo) {
+class NodeInfoHandler(val nodeInfo: NodeInfo) {
     fun index(request: ServerRequest): Mono<ServerResponse>
             = ok().body(Mono.just(nodeInfo))
 }
 
-class Router(val functionalHandler: FunctionalHandler) {
+class Router(val nodeInfoHandler: NodeInfoHandler) {
     fun router() = router() {
         accept(MediaType.APPLICATION_JSON).nest {
-                GET("/", functionalHandler::index)
+                GET("/", nodeInfoHandler::index)
         }
     }
 
@@ -47,7 +32,7 @@ class Router(val functionalHandler: FunctionalHandler) {
 class BlockchainKtWebfluxApplication
 
 fun beans() = beans {
-    bean<FunctionalHandler>()
+    bean<NodeInfoHandler>()
 
     bean {
         Router(ref()).router()
@@ -60,5 +45,5 @@ fun main(args: Array<String>) {
     SpringApplicationBuilder()
             .sources(BlockchainKtWebfluxApplication::class.java)
             .initializers(beans())
-            .run(*args);
+            .run(*args)
 }
