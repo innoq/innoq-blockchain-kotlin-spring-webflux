666cb5b23abb3ad301143b7d0f4a9aae87712786
Michael J. Simons <michael@simons.ac>
2018-04-19 13:02:48 +0200

    Create demo with 3 different ways to build a webflux application

--- src/main/kotlin/com/innoq/event/blockchainktwebflux/BlockchainKtWebfluxApplication.kt
+++ src/main/kotlin/com/innoq/event/blockchainktwebflux/BlockchainKtWebfluxApplication.kt
@@ -1,10 +1,16 @@
 package com.innoq.event.blockchainktwebflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.server.*
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -22,9 +28,34 @@ class WebfluxRestController {
     fun index(): Mono<NodeInfo> = Mono.just(NodeInfo("4711"))
 }
 
+class FunctionalHandler {
+    fun index(request: ServerRequest): Mono<ServerResponse>
+            = ok().body(Mono.just(NodeInfo("4711")))
+}
+
+class Router(val functionalHandler: FunctionalHandler) {
+    fun router() = router() {
+        accept(MediaType.APPLICATION_JSON).nest {
+                GET("/", functionalHandler::index)
+        }
+    }
+
+}
+
 @SpringBootApplication
 class BlockchainKtWebfluxApplication
 
+fun beans() = beans {
+    bean<FunctionalHandler>()
+
+    bean {
+        Router(ref()).router()
+    }
+}
+
 fun main(args: Array<String>) {
-    runApplication<BlockchainKtWebfluxApplication>(*args)
+    SpringApplicationBuilder()
+            .sources(BlockchainKtWebfluxApplication::class.java)
+            .initializers(beans())
+            .run(*args);
 }
