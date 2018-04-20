4efa6b873eda46d7c6324564ff3d562043189ce6
Michael J. Simons <michael@simons.ac>
2018-04-20 09:30:17 +0200

    Fix duplicate indizes

--- src/main/kotlin/com/innoq/event/blockchainktwebflux/domain/BlockChain.kt
+++ src/main/kotlin/com/innoq/event/blockchainktwebflux/domain/BlockChain.kt
@@ -1,10 +1,13 @@
 package com.innoq.event.blockchainktwebflux.domain
 
 import reactor.core.publisher.Flux
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicLong
 
 class BlockChain(genesisBlock: Block) {
 
-    val blocks: MutableList<Block> = mutableListOf(genesisBlock)
+    val blocks = mutableListOf(genesisBlock)
+    val lastIndex = AtomicLong(genesisBlock.index)
 
     val blockHeight: Int
         get() = this.blocks.size
@@ -12,10 +15,12 @@ class BlockChain(genesisBlock: Block) {
     fun mine() =
         Flux.generate<Block>{ sink -> sink.next(computeNextBlock()) }.doOnNext {blocks.add(it)}
 
-    internal fun computeNextBlock(timestamp: Long = System.currentTimeMillis()) =
-        generateSequence(0L) { it + 1 }
-                .map { blocks.last().newCandidate(timestamp, it) }
+    internal fun computeNextBlock(timestamp: Long = System.currentTimeMillis()) : Block {
+        val nextIndex = lastIndex.incrementAndGet()
+        return generateSequence(0L) { it + 1 }
+                .map { blocks.last().newCandidate(nextIndex, timestamp, it) }
                 .dropWhile { it.isValid().not() }
                 .first()
+    }
 }
 
