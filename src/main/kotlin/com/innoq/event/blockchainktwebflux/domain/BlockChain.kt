17ed26760b399f83ff56d28e88ebd921a1b5f379
Michael J. Simons <michael@simons.ac>
2018-04-20 18:03:05 +0200

    Parallelize mining

--- src/main/kotlin/com/innoq/event/blockchainktwebflux/domain/BlockChain.kt
+++ src/main/kotlin/com/innoq/event/blockchainktwebflux/domain/BlockChain.kt
@@ -1,21 +1,25 @@
 package com.innoq.event.blockchainktwebflux.domain
 
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+import reactor.core.scheduler.Schedulers
 import java.time.Clock
 import java.util.*
+import java.util.concurrent.LinkedBlockingDeque
 import java.util.concurrent.atomic.AtomicLong
+import java.util.stream.Stream
 
 class BlockChain(initialBlocks: List<Block>, private val eventPublisher: EventPublisher, private val clock: Clock, private val validBlockPrefix: String = "0000") {
 
     private val blocks = mutableListOf(*initialBlocks.toTypedArray())
     private val lastIndex = AtomicLong(initialBlocks.last().index)
-    private val pendingTransactions = mutableListOf<Transaction>()
+    private val pendingTransactions : Queue<Transaction> = LinkedBlockingDeque()
 
     val blockHeight: Int
         get() = this.blocks.size
 
-    fun mine() =
-            Mono.just(computeNextBlock()).doOnNext { blocks.add(it) }
+    fun mine() = computeNextBlock().doOnNext { blocks.add(it) }
 
     fun queue(payload: Payload) = Mono.fromSupplier {
         val pendingTransaction = Transaction(UUID.randomUUID().toString(), clock.millis(), payload)
@@ -28,23 +32,23 @@ class BlockChain(initialBlocks: List<Block>, private val eventPublisher: EventPu
             Mono.justOrEmpty(blocks.flatMap { it.transactions }.firstOrNull { it.id == transactionId }
                     ?: pendingTransactions.firstOrNull { it.id == transactionId })
 
-    private fun computeNextBlock(): Block {
+    private fun computeNextBlock(): Mono<Block> {
         val nextIndex = lastIndex.incrementAndGet()
         val timestamp = clock.millis()
         val transactions = selectTransactions(5)
 
-        return generateSequence(0L) { it + 1 }
+        return Flux.fromStream(Stream.iterate(0L, Long::inc))
+                .parallel()
+                .runOn(Schedulers.parallel())
                 .map { blocks.last().newCandidate(nextIndex, timestamp, it, transactions) }
-                .dropWhile { isValid(it).not() }
-                .first()
+                .filter{ isValid(it) }
+                .toMono()
     }
 
-    private fun selectTransactions(maxNumberOfTransactions: Int): List<Transaction> {
-        val transactions = pendingTransactions.take(maxNumberOfTransactions)
-        pendingTransactions.removeAll(transactions)
-
-        return transactions
-    }
+    private fun selectTransactions(maxNumberOfTransactions: Int): List<Transaction> =
+            generateSequence { pendingTransactions.poll() }
+                    .take(maxNumberOfTransactions)
+                    .filter { it != null }.toList()
 
     /**
      * A block is valid if its hash starts with n-zeros
