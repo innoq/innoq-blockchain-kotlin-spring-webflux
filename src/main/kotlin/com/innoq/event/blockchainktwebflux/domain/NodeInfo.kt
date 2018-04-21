a6059d297f94f770999ecefc8bd076e75a5531a0
Torsten Mandry <torsten.mandry@innoq.com>
2018-04-21 13:11:37 +0200

    remove unnecessary init block

--- src/main/kotlin/com/innoq/event/blockchainktwebflux/domain/NodeInfo.kt
+++ src/main/kotlin/com/innoq/event/blockchainktwebflux/domain/NodeInfo.kt
@@ -3,13 +3,8 @@ package com.innoq.event.blockchainktwebflux.domain
 import java.util.*
 
 class NodeInfo(_chain: BlockChain) {
-    val id: String
-    private val chain: BlockChain
-
-    init {
-        id = UUID.randomUUID().toString()
-        chain = _chain
-    }
+    val id = UUID.randomUUID().toString()
+    private val chain = _chain
 
     val currentBlockHeight: Int
         get() = this.chain.blockHeight
